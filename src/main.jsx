809358cce515c39765b6033b53bb1d8c972378d9
Cristian-Aguilera-316 <cristian.ap316@gmail.com>
2025-12-02 15:53:50 -0300

    Agregar basename para GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
-import { PhotoProvider } from "./context/PhotoContext";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import Home from './views/Home'
+import Favoritos from './views/Favorites'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PhotoProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </PhotoProvider>
+    <BrowserRouter basename="/Desafiolatam_M5D2">
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="favoritos" element={<Favoritos />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-);
+)
